d61ace4ac57a8c6c558e43c3e45842147010ca2a
chaiyasave101 <sherlockholmes.save@gmail.com>
2022-04-27 18:03:39 +0700

    Application
    Beta Test Application

---
+++ DUMBBELL/Application/smartdumbbell-flutter-master/lib/constants.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+const appbarColorColor = Color(0xFF0C1528);
+const buttonGetStartColor = Colors.blue;
+const buttonUnlockColor = Color(0xFFFF5151);
+const buttonConfirmColor = Color(0xFF2ECC71);
+
+const gestureBoxBorderColor = Color(0xFF707070);
+const gestureBoxShadowColor = Color(0x998A8888);
+const gestureBoxTitleColor = Color(0xFFAFAFAF);
+
+const defaultActionButtonColor = Color(0xFFACACAC);
---
+++ DUMBBELL/Application/smartdumbbell-flutter-master/lib/menu_screen/edit_gesture_screen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class EditGestureScreen extends StatefulWidget {
+  const EditGestureScreen({Key? key}) : super(key: key);
+
+  @override
+  State<EditGestureScreen> createState() => _EditGestureScreenState();
+}
+
+class _EditGestureScreenState extends State<EditGestureScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        child: Column(
+      children: [
+        Text("555"),
+        Text("555"),
+        Text("555"),
+        Text("555"),
+        Text("555"),
+        Text("555")
+      ],
+    ));
+  }
+}
